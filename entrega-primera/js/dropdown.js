9bf6fc0b315e7314291799d13e208fd9cce74c6a
root <root@server>
2022-06-27 18:23:31 -0300

    Pasando Cambios

---
+++ entrega-primera/js/dropdown.js
@@ -0,0 +1,1 @@
+function myFunction(){document.getElementById("myDropdown").classList.toggle("show")}window.onclick=function(d){if(!d.target.matches(".dropbtn")){var a,b=document.getElementsByClassName("dropdown-content");for(a=0;a<b.length;a++){var c=b[a];c.classList.contains("show")&&c.classList.remove("show")}}}
