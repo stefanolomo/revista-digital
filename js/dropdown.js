27871aafd7041e511aee2a3d7928f1097d17b440
stefanolomo <stefanolomo@gmail.com>
2022-11-10 18:13:24 -0300

    Auto header ready

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,8 +2,6 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show")
 }
 window.onclick = function(d) {
-    console.log(!d);
-    console.log(!d.target);
     if (!d.target.matches(".dropbtn")) {
         var a, b = document.getElementsByClassName("dropdown-content");
         for (a = 0; a < b.length; a++) {
