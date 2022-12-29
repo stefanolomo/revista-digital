7c907b866551d0c0ae7de8f3e4c4ce97c582a540
stefanolomo <stefanolomo@gmail.com>
2022-12-29 02:57:49 -0300

    Dropdown

--- js/btnManagement.js
+++ js/btnManagement.js
@@ -1,13 +1,23 @@
 function btnManagement() {
+	let DropdownButtons = document.querySelector(".section")
+	let SectionButton = document.getElementById("secciones")
+	let dropdown = document.querySelector(".dropdown")
 	let HomeButton = document.getElementById("inicio")
 	let host = window.location.host
 	let protocol = window.location.protocol
 	let pathname = window.location.pathname
+	document.addEventListener("click", ()=>{
+		if (!DropdownButtons.contains(event.target) && !dropdown.contains(event.target) && !SectionButton.contains(event.target)) {
+			dropdown.classList.add('hidden');
+		}
+	})
+	SectionButton.addEventListener("click", ()=>{
+		dropdown.classList.toggle("hidden")
+		dropdown.classList.toggle("fade-in")
+	})
 	if (pathname !== "/") {
-		console.log("test")
 		HomeButton.href = `${protocol}//${host}`
 	}else{
-		console.log("test2")
 		HomeButton.href = `#`
 	}
 	let RedaccionButton = document.getElementById("redaccion")
